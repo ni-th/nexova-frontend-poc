e407ebe24c26d6e377f1edc09669c2ce2b2b1b38
nimantha1999 <nimantha.bt@gmail.com>
2025-09-19 00:50:42 +0530

    add db detail shownig table

--- src/pages/ConfigManagent.tsx
+++ src/pages/ConfigManagent.tsx
@@ -1,50 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaAngleDown } from "react-icons/fa6";
 import axios from "axios";
 import Swal from "sweetalert2";
+import type { DBConfig } from "../types/config";
 
-interface DBConfig {
-  host: string;
-  username: string;
-  port: number | null;
-  password: string;
-  databaseName: string;
-}
 const ConfigManagent: React.FC = () => {
-  const [dbConfig, setDbConfig] = useState<DBConfig>({
-    host: "",
-    username: "",
-    port: null,
-    password: "",
-    databaseName: "",
+  // useStates
+  const [config, setConfig] = useState({
+    db: { host: "", username: "", port: null, password: "", databaseName: "" },
+    email: { api_key: "", sender_email: "" },
+    sms: { api_key: "", sender_phone: "" },
   });
 
-  // Handle input changes
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const [data, setData] = useState<DBConfig[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    setLoading(true); // start loading
+    setError(null); // clear previous errors
+    try {
+      const response = await axios.get<DBConfig[]>(
+        "http://localhost:8080/config/db/find-all"
+      );
+      setData(response.data);
+    } catch (err) {
+      setError(err.message || "Something went wrong");
+    } finally {
+      setLoading(false); // stop loading
+    }
+  };
+
+  useEffect(() => {
+    fetchData(); // fetch data when page loads
+  }, []);
+
+  //  input changes
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    type: "db" | "email" | "sms"
+  ) => {
     const { id, value } = e.target;
-    setDbConfig((prev) => ({
+    setConfig((prev) => ({
       ...prev,
-      [id]: id === "port" ? Number(value) : value,
+      [type]: {
+        ...prev[type],
+        [id]: id === "port" ? Number(value) : value,
+      },
     }));
   };
-  // Handle form submission
-  const handleSubmit = async (e: React.FormEvent) => {
+
+  // form submitions
+  const handleSubmit = async (
+    e: React.FormEvent,
+    type: "db" | "email" | "sms"
+  ) => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:8080/config/db/add",
-        dbConfig
+        `http://localhost:8080/config/${type}/add`,
+        config[type]
       );
       console.log("Config sent:", response.data);
       Swal.fire({
-        title: "DB configurations saved successfully !",
+        title: `${type} configurations saved successfully !`,
         icon: "success",
         draggable: true,
       });
     } catch (error) {
       console.error("Error sending config:", error);
       Swal.fire({
-        title: "DB configurations not saved !",
+        title: `${type} configurations not saved !`,
         icon: "error",
         draggable: true,
       });
@@ -69,103 +95,139 @@ const ConfigManagent: React.FC = () => {
         className="hidden"
         aria-labelledby="accordion-collapse-heading-1"
       >
-        <form
-          onSubmit={handleSubmit}
-          className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900"
-        >
-          <div className="grid gap-6 mb-6 md:grid-cols-2">
-            <div>
-              <label
-                htmlFor="host"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Host
-              </label>
-              <input
-                value={dbConfig.host}
-                onChange={handleChange}
-                type="text"
-                id="host"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="localhost"
-                required
-              />
+        <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+          <form onSubmit={(e) => handleSubmit(e, "db")} className="">
+            <div className="grid gap-6 mb-6 md:grid-cols-2">
+              <div>
+                <label
+                  htmlFor="host"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Host
+                </label>
+                <input
+                  value={config.db.host}
+                  onChange={(e) => handleChange(e, "db")}
+                  type="text"
+                  id="host"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="localhost"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="username"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  User Name
+                </label>
+                <input
+                  value={config.db.username}
+                  onChange={(e) => handleChange(e, "db")}
+                  type="text"
+                  id="username"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="doe"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="port"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Port
+                </label>
+                <input
+                  value={config.db.port ?? ""}
+                  onChange={(e) => handleChange(e, "db")}
+                  type="number"
+                  id="port"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="3306"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  value={config.db.password}
+                  onChange={(e) => handleChange(e, "db")}
+                  type="password"
+                  id="password"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="databaseName"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Database
+                </label>
+                <input
+                  value={config.db.databaseName}
+                  onChange={(e) => handleChange(e, "db")}
+                  type="text"
+                  id="databaseName"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="mydb"
+                  required
+                />
+              </div>
             </div>
-            <div>
-              <label
-                htmlFor="username"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                User Name
-              </label>
-              <input
-                value={dbConfig.username}
-                onChange={handleChange}
-                type="text"
-                id="username"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="doe"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="port"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Port
-              </label>
-              <input
-                value={dbConfig.port ?? ""}
-                onChange={handleChange}
-                type="number"
-                id="port"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="3306"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Password
-              </label>
-              <input
-                value={dbConfig.password}
-                onChange={handleChange}
-                type="password"
-                id="password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="databaseName"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Database
-              </label>
-              <input
-                value={dbConfig.databaseName}
-                onChange={handleChange}
-                type="text"
-                id="databaseName"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="mydb"
-                required
-              />
-            </div>
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          </form>
+          {loading && <p>Loading...</p>}
+          {error && <p>Error: {error}</p>}
+          <div className="relative overflow-x-auto mt-4">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" className="px-6 py-3">
+                    Database
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Host
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Port
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Username
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item) => (
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      {item.databaseName}
+                    </th>
+                    <td className="px-6 py-4">{item.host}</td>
+                    <td className="px-6 py-4">{item.port}</td>
+                    <td className="px-6 py-4">{item.username}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
-          <button
-            type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Submit
-          </button>
-        </form>
+        </div>
       </div>
 
       <h2 id="accordion-collapse-heading-2">
