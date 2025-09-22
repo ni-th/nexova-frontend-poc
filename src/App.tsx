ba3f408145755434ce3e5ab53dda4a523af328b2
nimantha1999 <nimantha.bt@gmail.com>
2025-09-22 11:24:51 +0530

    update the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import AppNavbar from "./components/layout/AppNavbar";
 import AppSideBar from "./components/layout/AppSideBar";
 import "flowbite";
@@ -9,21 +9,18 @@ import ConfigManagent from "./pages/ConfigManagent";
 import SecurityManagement from "./pages/SecurityManagement";
 import UserManagement from "./pages/UserManagement";
 const App = () => {
-  const location = useLocation();
-
-  useEffect(() => {
+    useEffect(() => {
     initFlowbite();
-  }, [location]);
+  }, [useLocation()]);
   return (
     <>
       <AppNavbar />
-      <div className="flex m-2">
+      <div className="flex m-2 justify-center">
         <div className="w-64 flex-none h-full">
           <AppSideBar />
         </div>
-        <div className="w-64 flex-1">
+        <div className="w-150 flex-initial  min-w-80">
           <Routes>
-            <Route path="/" element={<ConfigManagent />}></Route>
             <Route path="/config" element={<ConfigManagent />}></Route>
             <Route path="/user-management" element={<UserManagement />}></Route>
             <Route path="/security" element={<SecurityManagement />}></Route>
